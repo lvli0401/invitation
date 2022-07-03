e1a638469a573f88f8fd379764d41fa7a3f24f49
muying <muying@pinduoduo.com>
2022-07-03 20:28:25 +0800

    fix: jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  // enzyme配置文件位置
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss)$': '<rootDir>/__mocks__/styleMock.js',
   },
   transform: {
     '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest',
+    '.+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
   },
   globals: {
     'ts-jest': {
@@ -13,7 +18,7 @@ module.exports = {
   // 是否显示覆盖率报告
   collectCoverage: true,
   // 告诉 jest 哪些文件需要经过单元测试测试
-  collectCoverageFrom: ['src/pages/index.ts'],
+  collectCoverageFrom: ['src/pages/index/index'],
   coverageThreshold: {
     global: {
       statements: 90, // 保证每个语句都执行了
--- test/home/index.test.js
+++ test/home/index.test.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 import Index from '@/pages/index/index'
-import Enzyme, { shallow, render, mount } from 'enzyme'
-
-Enzyme.configure({
-  adapter: new Adapter(),
-})
+import { shallow, render, mount } from 'enzyme'
 
 //组件渲染测试
 describe(`测试<App /> snapshots`, () => {
   const tree = shallow(<Index />)
 
-  it('1. 匹配快照', () => {
+  it('1. 匹配首页快照', () => {
     expect(toJson(tree)).toMatchSnapshot()
+  })
+
+  it('2. 测试点击invite按钮', () => {
+    const wrapper = mount(<Index />)
+    const button = wrapper.find('button').at(0)
+
+    expect(button.exists)
+    button.simulate('click')
+    expect(wrapper.state('visible')).toBe(true)
   })
 })
